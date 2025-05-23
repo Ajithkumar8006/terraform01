8c523220ad417e47382bb2842bd3c5762a61a25e
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-05-24 00:34:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  tags = ["web", "dev"]
+}
